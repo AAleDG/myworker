41a2f920e2e3f7f1113a8e12a98e972924386a03
Alessandro Del Gaudio <alessandroantonio.delgaudio@emea.nttdata.com>
2025-09-28 20:44:04 +0200

    feat: fix registration state and bs

--- myworkers/lib/core/l10n/delegates/core_localizations.dart
+++ myworkers/lib/core/l10n/delegates/core_localizations.dart
@@ -62,7 +62,8 @@ import 'core_localizations_it.dart';
 /// be consistent with the languages listed in the CoreLocalizations.supportedLocales
 /// property.
 abstract class CoreLocalizations {
-  CoreLocalizations(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+  CoreLocalizations(String locale)
+      : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -70,7 +71,8 @@ abstract class CoreLocalizations {
     return Localizations.of<CoreLocalizations>(context, CoreLocalizations)!;
   }
 
-  static const LocalizationsDelegate<CoreLocalizations> delegate = _CoreLocalizationsDelegate();
+  static const LocalizationsDelegate<CoreLocalizations> delegate =
+      _CoreLocalizationsDelegate();
 
   /// A list of this localizations delegate along with the default localizations
   /// delegates.
@@ -82,7 +84,8 @@ abstract class CoreLocalizations {
   /// Additional delegates can be added by appending to this list in
   /// MaterialApp. This list does not have to be used at all if a custom list
   /// of delegates is preferred or required.
-  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
     delegate,
     GlobalMaterialLocalizations.delegate,
     GlobalCupertinoLocalizations.delegate,
@@ -172,36 +175,80 @@ abstract class CoreLocalizations {
   /// In it, this message translates to:
   /// **'Accedi'**
   String get loginPageAccessButtonText;
+
+  /// No description provided for @registrationPageTitle.
+  ///
+  /// In it, this message translates to:
+  /// **'Registra i tuoi dati'**
+  String get registrationPageTitle;
+
+  /// No description provided for @registrationPageHintName.
+  ///
+  /// In it, this message translates to:
+  /// **'Inserisci il tuo nome'**
+  String get registrationPageHintName;
+
+  /// No description provided for @registrationPageHintSurname.
+  ///
+  /// In it, this message translates to:
+  /// **'Inserisci il tuo cognome'**
+  String get registrationPageHintSurname;
+
+  /// No description provided for @registrationPageHintCF.
+  ///
+  /// In it, this message translates to:
+  /// **'Inserisci il tuo CF'**
+  String get registrationPageHintCF;
+
+  /// No description provided for @registrationPageHintGender.
+  ///
+  /// In it, this message translates to:
+  /// **'Seleziona il tuo genere'**
+  String get registrationPageHintGender;
+
+  /// No description provided for @registrationPageHintPhoneConfirm.
+  ///
+  /// In it, this message translates to:
+  /// **'Inserisci il numero di conferma'**
+  String get registrationPageHintPhoneConfirm;
+
+  /// No description provided for @registrationPageHintPhone.
+  ///
+  /// In it, this message translates to:
+  /// **'Inserisci il tuo numero di telefono'**
+  String get registrationPageHintPhone;
 }
 
-class _CoreLocalizationsDelegate extends LocalizationsDelegate<CoreLocalizations> {
+class _CoreLocalizationsDelegate
+    extends LocalizationsDelegate<CoreLocalizations> {
   const _CoreLocalizationsDelegate();
 
   @override
   Future<CoreLocalizations> load(Locale locale) {
-    return SynchronousFuture<CoreLocalizations>(lookupCoreLocalizations(locale));
+    return SynchronousFuture<CoreLocalizations>(
+        lookupCoreLocalizations(locale));
   }
 
   @override
-  bool isSupported(Locale locale) => <String>['en', 'it'].contains(locale.languageCode);
+  bool isSupported(Locale locale) =>
+      <String>['en', 'it'].contains(locale.languageCode);
 
   @override
   bool shouldReload(_CoreLocalizationsDelegate old) => false;
 }
 
 CoreLocalizations lookupCoreLocalizations(Locale locale) {
-
-
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
-    case 'en': return CoreLocalizationsEn();
-    case 'it': return CoreLocalizationsIt();
+    case 'en':
+      return CoreLocalizationsEn();
+    case 'it':
+      return CoreLocalizationsIt();
   }
 
   throw FlutterError(
-    'CoreLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-    'an issue with the localizations generation tool. Please file an issue '
-    'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.'
-  );
+      'CoreLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+      'an issue with the localizations generation tool. Please file an issue '
+      'on GitHub with a reproducible sample app and the gen-l10n configuration '
+      'that was used.');
 }
--- myworkers/lib/features/registration/presentations/registration_screen.dart
+++ myworkers/lib/features/registration/presentations/registration_screen.dart
@@ -1,5 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:myworkers/core/l10n/l10n_ext.dart';
+import 'package:myworkers/features/registration/cubit/registration_cubit.dart';
+
+import '../../../core/common_widgets/export_widgets.dart';
 
 @RoutePage()
 class RegistrationScreen extends StatelessWidget {
@@ -7,21 +12,216 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.blue[800]!,
-              Colors.red[800]!,
-            ],
-          ),
-        ),
-        child: const Center(
-          child: Text(
-            'Registration Screen',
-            style: TextStyle(color: Colors.white, fontSize: 24),
+    final GlobalKey<FormState> keys = GlobalKey<FormState>();
+    return BlocBuilder<RegistrationCubit, RegistrationState>(
+      builder: (context, state) => Form(
+        key: keys,
+        child: Scaffold(
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blue[800]!,
+                  Colors.red[800]!,
+                ],
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.1,
+                    ),
+                    AppText(
+                      context.l10nCore.registrationPageTitle,
+                      maxLines: 2,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    AppTextField(
+                      controller:
+                          context.read<RegistrationCubit>().emailController,
+                      hintText: context.l10nCore.loginPageEmailHint,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return context
+                              .l10nCore.loginPageHintTextErrorEmptyEmail;
+                        } else if (!value.contains('@') ||
+                            !value.contains('.')) {
+                          return context.l10nCore.loginPageHintTextValidEmail;
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    AppTextField(
+                      controller:
+                          context.read<RegistrationCubit>().nameController,
+                      hintText: context.l10nCore.registrationPageHintName,
+                      onPressed: () {},
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return context
+                              .l10nCore.loginPageHintTextErrorEmptyPassword;
+                        } else if (value.length <= 8) {
+                          return context
+                              .l10nCore.loginPageHintTextValidPassword;
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    AppTextField(
+                      controller:
+                          context.read<RegistrationCubit>().surnameController,
+                      hintText: context.l10nCore.registrationPageHintSurname,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return context
+                              .l10nCore.loginPageHintTextErrorEmptyEmail;
+                        } else if (!value.contains('@') ||
+                            !value.contains('.')) {
+                          return context.l10nCore.loginPageHintTextValidEmail;
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    AppTextField(
+                      controller:
+                          context.read<RegistrationCubit>().cfController,
+                      hintText: context.l10nCore.registrationPageHintCF,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return context
+                              .l10nCore.loginPageHintTextErrorEmptyEmail;
+                        } else if (!value.contains('@') ||
+                            !value.contains('.')) {
+                          return context.l10nCore.loginPageHintTextValidEmail;
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    AppTextField(
+                      controller: context
+                          .read<RegistrationCubit>()
+                          .confirmPasswordController,
+                      hintText: context.l10nCore.registrationPageHintGender,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return context
+                              .l10nCore.loginPageHintTextErrorEmptyEmail;
+                        } else if (!value.contains('@') ||
+                            !value.contains('.')) {
+                          return context.l10nCore.loginPageHintTextValidEmail;
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    AppTextField(
+                      controller:
+                          context.read<RegistrationCubit>().phoneController,
+                      hintText: context.l10nCore.registrationPageHintPhone,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return context
+                              .l10nCore.loginPageHintTextErrorEmptyEmail;
+                        } else if (!value.contains('@') ||
+                            !value.contains('.')) {
+                          return context.l10nCore.loginPageHintTextValidEmail;
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    //TODO TOGLIERE IL NUMERO DI CONFERMA ED FAR ARRIVARE AL TELEFONO DI ALE IL CODICE DI CONFERMA
+                    AppTextField(
+                      controller: context
+                          .read<RegistrationCubit>()
+                          .phoneConfirmController,
+                      hintText:
+                          context.l10nCore.registrationPageHintPhoneConfirm,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return context
+                              .l10nCore.loginPageHintTextErrorEmptyEmail;
+                        } else if (!value.contains('@') ||
+                            !value.contains('.')) {
+                          return context.l10nCore.loginPageHintTextValidEmail;
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: state.showPassword,
+                          activeColor: Colors.blue,
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 2,
+                          ),
+                          onChanged: (_) {
+                            //context.read<LoginCubit>().updateDataaccess();
+                          },
+                        ),
+                        AppText(
+                          context.l10nCore.loginPageRememberMe,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          maxLines: 1,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    AppButton(
+                      onPressed: () {
+                        if (keys.currentState!.validate()) {
+                          // context.read<LoginCubit>().checkStatus(
+                          //       context,
+                          //     );
+                        }
+                      },
+                      textButton: 'Registrati',
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
